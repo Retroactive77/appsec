3e18d82c0f21687564d75d09d416fee21da8988a
Retroactive77 <122386044+Retroactive77@users.noreply.github.com>
2023-01-28 02:40:17 +0800

    Add files via upload

--- backend.py
+++ backend.py
@@ -1,22 +1,32 @@
 import shelve
 from wtforms import Form, StringField, EmailField, PasswordField, validators,ValidationError
 
-class Create_useraccount(Form):
-    name=StringField('entername',[validators.DataRequired()])
-    email=EmailField('enteremail',[validators.DataRequired()])
+
+class CreateUseraccount(Form):
+    name=StringField('',[validators.DataRequired()],render_kw={"placeholder":"Enter your name"})
+    email=EmailField('',[validators.DataRequired()],render_kw={"placeholder":"Enter your email"})
+    password=PasswordField('',[validators.DataRequired()],render_kw={"placeholder":"Enter your password"})
+    repeatpassword=PasswordField('',[validators.DataRequired(),validators.EqualTo('password',message='Passwords must match')],render_kw={"placeholder":"Re-enter password"})
+
+
+class LoginUseraccount(Form):
+    email=EmailField('',[validators.DataRequired()],render_kw={"placeholder": "Enter your email address"})
+    password=PasswordField('',[validators.DataRequired()],render_kw={"placeholder": "Enter your password"})
+
+
+class ForgetUserpassword(Form):
+    email=EmailField('',[validators.DataRequired()],render_kw={"placeholder": "Enter your email address"})
+
+    def validate_email(self,field):
+        if field.data not in list(user_dict.keys()):
+            raise ValidationError("Email is not registered. Please try again")
+
+
+class ResetUserpassword(Form):
     password=PasswordField('enterpassword',[validators.DataRequired()])
     repeatpassword=PasswordField('enterrepeatpassword',[validators.DataRequired(),validators.EqualTo('password',message='Passwords must match')],)
 
-class Login_useraccount(Form):
-    email=EmailField('enteremail',[validators.DataRequired()])
-    password=PasswordField('enterpassword',[validators.DataRequired()])
 
-class Reset_useraccount(Form):
-    email=EmailField('enteremail',[validators.DataRequired()])
-
-    def validate_email(self,form,field):
-        if field.data not in list(user_dict.keys()):
-            raise ValidationError("Email is not registered. Please try again")
 class User:
     def __init__(self,username,email,password):
         self.__username=username
@@ -25,15 +35,19 @@ class User:
 
     def get_username(self):
         return self.__username
+
     def get_email(self):
         return self.__email
+
     def get_password(self):
         return self.__password
 
     def set_username(self,username):
         self.__username=username
+
     def set_email(self,email):
         self.__email=email
+
     def set_password(self,password):
         self.__password=password
 
@@ -44,12 +58,7 @@ def create_list():
     except:
         print('error')
     else:
-
-
         user_dict=db['key']
         listofusers=list(user_dict.values())
         db.close()
         return listofusers
-
-
-
--- main.py
+++ main.py
@@ -1,45 +1,62 @@
 import shelve,backend
-from flask import Flask, render_template, request,redirect,url_for
+from flask import Flask, render_template, request,redirect,url_for,session
+from flask_session import Session
 
 
 
-app = Flask(__name__)
+
+
+app = Flask(__name__,static_folder='static')
+
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 @app.route('/')
 def home():
-    return render_template('base.html')
+    if session['loggedin']==True:
+        return redirect(url_for('homepage'))
+    return render_template('home.html')
+
+@app.route('/home')
+def homepage():
+    #admin diff homepage?
+    if session['loggedin']==False:
+        return redirect(url_for('home'))
+    return render_template('home.html',session=session['loggedin'],name=session['name'].get_username())
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    create_account_form=backend.Create_useraccount(request.form)
+    create_account_form=backend.CreateUseraccount(request.form)
     if request.method == 'POST' and create_account_form.validate():
-
         db = shelve.open('users','c')
-        try:
+        if 'key' in db:
             user_dict=db['key']
-        except:
-            print('empty shelve?')
+        else:
             user_dict={}
 
+        if create_account_form.email.data in user_dict:
+            return render_template('signup.html',form=create_account_form,emailused="Email is already in use")
+        else:
 
-        user_account=backend.User(create_account_form.name.data,create_account_form.email.data,create_account_form.password.data)
+            user_account=backend.User(create_account_form.name.data,create_account_form.email.data,create_account_form.password.data)
 
-
-
-        user_dict[user_account.get_email()]=user_account
-        db['key']=user_dict
-
-        db.close()
+            user_dict[user_account.get_email()]=user_account
+            db['key']=user_dict
+            session['name']=user_dict[create_account_form.email.data]
+            session['loggedin']=True
+            db.close()
 
         return redirect(url_for('data'))
 
 
 
-    return render_template('main.html',form=create_account_form)
+    return render_template('signup.html',form=create_account_form)
 
 @app.route('/login', methods=["GET","POST"])
 def login():
-    login_account_form=backend.Login_useraccount(request.form)
+    login_account_form=backend.LoginUseraccount(request.form)
     if request.method == 'POST' and login_account_form.validate():
 
         db = shelve.open('users','r')
@@ -51,7 +68,10 @@ def login():
         else:
             if login_account_form.email.data in list(user_dict.keys()):
                 if login_account_form.password.data == user_dict[login_account_form.email.data].get_password():
-                    return redirect('userprofile/'+login_account_form.email.data)
+                    session['name']=user_dict[login_account_form.email.data]
+                    session['loggedin']=True
+                    return redirect(url_for('homepage'))
+
                 else:
                     print("password wrong")
                     #password wrong
@@ -62,21 +82,50 @@ def login():
         finally:
             db.close()
 
-        return render_template('login.html',form=login_account_form)
+
+    return render_template('login.html',form=login_account_form)
+
+
+@app.route('/logout')
+def logout():
+    session['loggedin']=False
+    session['name']=None
+    return redirect(url_for('home'))
+
+@app.route('/check')
+def index():
+    if 'loggedin' in session:
+        return 'User is authenticated'+session['name'].get_username()
+    else:
+        return 'User is not authenticated'
 
 @app.route('/forgetpassword')
-def resetp():
-    reset_account_form=backend.Reset_useraccount(request.form)
-    if request.method == 'POST' and reset_account_form.validate():
+def forgetp():
+    forget_account_form=backend.ForgetUserpassword(request.form)
+    if request.method == 'POST' and forget_account_form.validate():
          db = shelve.open('users','r')
          try:
             user_dict=db['key']
          except:
             print('empty shelve?')
-
          else:
-            if login_account_form.email.data in list(user_dict.keys()):
-                pass
+            if forget_account_form.email.data in list(user_dict.keys()):
+                pass#send the email
+    return render_template('forgotpassword.html',form=forget_account_form)
+
+@app.route('/resetpassword')
+def resetp():
+    reset_password_form=backend.ResetUserpassword(request.form)
+    if request.method == 'POST' and reset_password_form.validate():
+        email=request.view_args['variable']
+        db = shelve.open('users','c')
+
+        user_dict=db['key']
+        user=user_dict[email]
+        user.set_password(reset_password_form.password.data)
+
+        db.close()
+
 
 
 @app.route('/index')
@@ -85,13 +134,34 @@ def data():
     count=len(users_list)
     return render_template('index.html',count=count,users_list=users_list)
 
-@app.route('/updateUser/<email>')
-def adminchangeuser():
-    return render_template()
+@app.route('/delete', methods=["GET","POST"])
+def deleteacc():
+    delete_account_form=backend.LoginUseraccount(request.form)
+    if request.method == 'POST' and delete_account_form.validate():
+        email=session['name'].get_email()
+        password=session['name'].get_password()
+        if email == delete_account_form.email.data and password == delete_account_form.password.data:
+            db = shelve.open('users','c')
 
-@app.route('/profile/<email>')
+            user_dict=db['key']
+            user_dict.pop(delete_account_form.email.data)
+
+            db['key']=user_dict
+            db.close()
+
+            session['name']=None
+            return redirect(url_for('home'))
+        else:
+            print('details wrong')
+    return render_template('deleteaccount.html',form=delete_account_form)
+@app.route('/deleteuser')
+def admindeleteuser():
+    pass
+    #return render_template()
+
+@app.route('/profile')
 def userprofile():
-    return
+    return render_template('profile.html',name=session['name'].get_username())
 
 if __name__ == '__main__':
     app.run()
