5580faead3646414fe7ae937f252ae7d3d34babc
Gibson Lee Guangqi <gibsonlee03@gmail.com>
2023-07-29 23:23:03 +0800

    saturday night

--- main.py
+++ main.py
@@ -39,8 +39,7 @@ app.config['SESSION_COOKIE_SECURE'] = True
 app.config['SESSION_COOKIE_HTTPONLY'] = True
 Session(app)
 
-auth=firebase.auth()
-
+pauth=firebase.auth()
 
 @app.after_request
 def add_security_headers(response):
@@ -52,69 +51,93 @@ def add_security_headers(response):
 
 @app.route('/')
 def home():
-    try:
-        backend.refresh_token()
-        print(user['idtoken'])
-        print(auth.get_account_info())
-    except:
-        print('token expired')
-    finally:
-        return render_template('homepg.html')
+
+    return render_template('homepg.html')
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if 'user' in session:
-            return redirect(url_for('/'))
-    create_account_form=backend.CreateUseraccount(request.form)
-    if request.method == 'POST' and create_account_form.validate():
+            return redirect(url_for('home'))
+    if request.method == 'POST':
 
-        name=create_account_form.name.data
-        email=create_account_form.email.data
-        password=create_account_form.password.data
+        name = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
 
         print(name,email,password)
 
         try:
-            auth.create_user_with_email_and_password(email, password)
-            auth.send_email_verification(email)
-        except:
-            print('error creating account')
+            pauth.create_user_with_email_and_password(email, password)
+            pauth.send_email_verification(email)
+        except pyrebase.pyrebase.HTTPError as e:
+            error_message = e.args[1]['error']['message']
+            if error_message == 'EMAIL_EXISTS':
+                print("Email address is already in use.")
+            elif error_message == 'INVALID_EMAIL':
+                print("Invalid email address format.")
+            elif error_message == 'WEAK_PASSWORD':
+                print("Weak password. It should be at least 6 characters long.")
+            elif error_message == 'INVALID_PHONE_NUMBER':
+                print("Invalid phone number format.")
+            elif error_message == 'USER_DISABLED':
+                print("The user account has been disabled by an administrator.")
+            elif error_message == 'TOO_MANY_ATTEMPTS_TRY_LATER':
+                print("Too many sign-up attempts. Please try again later.")
+            else:
+                print("An unknown error occurred:", error_message)
 
         else:
-            session['user']=email
+            db = firebase.database()
+            data = {
+                "name":name,
+                "email":email
+            }
+
+            decoded_token = auth.verify_id_token(user['idToken'])
+            uid = decoded_token['uid']
+            results = db.child("users").child(uid).push(data,user['idToken'])
+
+            session['name']=name
+            session['email']= email
+
+            print('something')
 
         return redirect(url_for('home'))
 
 
 
-    return render_template('signup.html',form=create_account_form)
+    return render_template('signup.html')
 
 @app.route('/login', methods=["GET","POST"])
 def login():
-    login_account_form=backend.LoginUseraccount(request.form)
-    if request.method == 'POST' and login_account_form.validate():
-        email=login_account_form.email.data
-        password=login_account_form.password.data
+    if request.method == 'POST':
+
+        email = request.form.get('email')
+        password= request.form.get('password')
+
         try:
-            user = auth.sign_in_with_email_and_password(email, password)
+            user = pauth.sign_in_with_email_and_password(email, password)
         except:
             print('wrong credentials')
         else:
+            decoded_token = auth.verify_id_token(user['idToken'])
+            uid = decoded_token['uid']
+
+            info = db.child('users').child(uid).get()
+
+            session['name'] = info['name']
+            session['email'] = info['email']
 
 
-            session['user']=email
-
-
-            print(auth.get_account_info(user['idToken']))
+            print(pauth.get_account_info(user['idToken']))
             return redirect(url_for('home'))
-    return render_template('login.html',form=login_account_form)
+    return render_template('login.html')
 
 
 @app.route('/logout')
 def logout():
     session.clear()
-
     return redirect(url_for('home'))
 
 @app.route('/forgetpassword',methods=["GET","POST"])
@@ -122,7 +145,7 @@ def forgetp():
     forget_account_form=backend.ForgetUserpassword(request.form)
     if request.method == 'POST' and forget_account_form.validate():
         email=forget_account_form.email.data
-        auth.send_password_reset_email(email)
+        pauth.send_password_reset_email(email)
     return render_template('forgotpassword.html',form=forget_account_form)
 
 @app.route('/changepassword', methods=["GET","POST"])
@@ -132,7 +155,7 @@ def resetp():
     reset_password_form=backend.ResetUserpassword(request.form)
     if request.method == 'POST' and reset_password_form.validate():
         try:
-            auth.sign_in_with_email_and_password(session['user'],reset_password_form.oldpassword.data)
+            pauth.sign_in_with_email_and_password(session['user'], reset_password_form.oldpassword.data)
         except KeyError:
             return render_template('resetpassword.html',form=reset_password_form,passw="Password(s) do not meet requirements.")
 
@@ -158,8 +181,7 @@ def deleteacc():
     try:
         password=0
         if 'tokenid' in user and password==password:
-            session.pop('user')
-            auth.delete_user_account(user['tokenid'])
+            session.clear()
     finally:
         return redirect(url_for('home'))
 
@@ -170,21 +192,31 @@ def admindeleteuser(user):
             session['loggedin']=False
             session['name']=None
 
-    db = shelve.open('users','c')
-    user_dicts=db['key']
-    user_dicts.pop(user)
-
-    db['key']=user_dicts
-    db.close()
-
-    return redirect(url_for('data'))
 
 @app.route('/profile')
 def userprofile():
-    if session['loggedin']==False:
-        return redirect(url_for('home'))
-    return render_template('profile.html',session=session['loggedin'],name=session['name'].get_username(),email=session['name'].get_email(),pword=session['name'].get_password())
+    return render_template('profile.html')
 
+@app.route('/create-admin')
+def adminlogin():
+    login_account_form=backend.LoginUseraccount(request.form)
+    if request.method == 'POST' and login_account_form.validate():
+        email=login_account_form.email.data
+        password=login_account_form.password.data
+        admin=''
+        try:
+            user = pauth.sign_in_with_email_and_password(email, password)
+        except:
+            print('wrong credentials')
+        else:
+
+
+            session['user']=email
+
+
+            print(pauth.get_account_info(user['idToken']))
+            return redirect(url_for('home'))
+    return render_template('login.html',form=login_account_form)
 
 
 if __name__ == '__main__':
